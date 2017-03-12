23773795f7f8c55d247ef06a54aac9253798c7c7
Konstantin Evchenko <evchenk2@illinois.edu>
2017-03-12 18:21:02 -0500

    Add signup and login/logout routes

--- TeamMatcher/views.py
+++ TeamMatcher/views.py
@@ -5,36 +5,54 @@ from TeamMatcher.student.student import Student
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    if 'username' in session:
+        return render_template('home.html')
+
+    return redirect(url_for('login'))
 
 
-@app.route('/signup',methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def signUp():
     """Show sign up page or sign the student up"""
-    error = None
+    if 'username' in session:
+        redirect(url_for('index'))
 
+    error = None
     # sign the user up and redirect to the main page
     if request.method == 'POST':
-        if Student.exists(request['username']):
+        if Student.exists(request.form['username']):
             error = 'User with this username already exists'
         else:
             # add the user into the database, store the session and redirect
+            try:
+                Student.add(request.form['username'],
+                            request.form['password'])
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            except RuntimeError as err:
+                error = err
 
-            return redirect(url_for('index'))
     return render_template('signup.html', error)
 
 @app.route('/login', methods=['POST'])
 def login():
     """Try to login the user"""
+    if 'username' in session:
+        return redirect(url_for('index'))
+
     error = None
     if request.method == 'POST':
-        if not Student.exists(request['username']):
+        if not Student.exists(request.form['username']):
             error = 'User with this username does not exist'
-        elif Student.verify(request['username'], request['password']):
-            # add session
+        elif Student.verify(request.form['username'], request.form['password']):
+            session['username'] = request.form['username']
             return redirect(url_for('index'))
         else:
             error = 'Invalid password'
 
     return render_template('login.html', error=error)
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
